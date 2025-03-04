0a01d91b6330ecd7af1a1dc6e883144e07ab9cff
miccza750 <120047283+miccza750@users.noreply.github.com>
2025-03-04 09:24:40 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Tasks task = new Tasks();
+       // task.Tasks1();
+        task.Tasks4();
+
+    }
+}
